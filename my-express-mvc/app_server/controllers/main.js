bb28df8c3dd3440aec4044741a3a1aa989c48128
Valens Florencia <valensflorencia_2226240038@mhs.mdp.ac.id>
2024-10-11 18:30:27 +0700

    Express MVC
    Membuat Express MVC

---
+++ my-express-mvc/app_server/controllers/main.js
@@ -0,0 +1,14 @@
+//controller index
+const index = (req, res) => {
+    res.render("index", {title: "Express MVC"});
+}
+//controller kontak
+const kontak = (req, res) => {
+    res.render("kontak", {title: "Ini Halaman Kontak"});
+}
+
+//controller profile
+const profile = (req, res) => {
+    res.render("profile", {title: "ini Halaman Profile"})
+}
+module.exports = {index, kontak, profile};
---
+++ my-express-mvc/app_server/routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+//panggil controller main
+var mainController = require("../controllers/main");
+
+/* GET home page. */
+router.get('/', mainController.index);
+router.get('/kontak', mainController.kontak);
+router.get('/profile', mainController.profile);
+
+module.exports = router;
