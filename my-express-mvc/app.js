a9eeaf3d6a838377f71d7b76b10e609dcf458f79
ValensFlorencia <160854619+ValensFlorencia@users.noreply.github.com>
2024-10-18 18:33:13 +0700

    Building a Data Model with MongoDB and Mongoose
    DONE

--- my-express-mvc/app.js
+++ my-express-mvc/app.js
@@ -3,17 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-//load mongodb db connection
 require('./app_server/models/db');
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
+var mahasiswaRouter = require('./app_server/routes/mahasiswas');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'app_server','views'));
+app.set('views', path.join(__dirname,'app_server', 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
@@ -24,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api', mahasiswaRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
