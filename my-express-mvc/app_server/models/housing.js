dc070e5acba213d487f19fbd78fd72bf0eefc611
ValensFlorencia <160854619+ValensFlorencia@users.noreply.github.com>
2024-11-27 15:32:22 +0700

    n

---
+++ my-express-mvc/app_server/models/housing.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const housingSchema = new mongoose.Schema({
+    id: {
+        type: Number, 
+        required: true
+    }, 
+    name: {
+        type: String,
+        required: true
+    }, 
+    city: {
+        type: String,
+        required: true
+    }, 
+    state: {
+        type: String, 
+        required: true
+    }, 
+    photo: {
+        type: String,
+        required: true
+    }, 
+    availableUnits: {
+        type: Number,
+        required: true
+    }, 
+    wifi: {
+        type: Boolean,
+        required: true
+    },
+    laundry: {
+        type: Boolean,
+        required: true
+    },
+});
+
+const Housing = mongoose.model('Hosuing', housingSchema);
+module.exports = Housing;
