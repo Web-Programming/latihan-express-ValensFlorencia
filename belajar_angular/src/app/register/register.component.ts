a479fdbb46fd7c76cce6b509e4e54898224fbacd
ValensFlorencia <160854619+ValensFlorencia@users.noreply.github.com>
2024-12-04 20:25:55 +0700

    COMMIT
    4des2024

--- belajar_angular/src/app/register/register.component.ts
+++ belajar_angular/src/app/register/register.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../authentication.service';
 
 @Component({
   selector: 'app-register',
@@ -14,6 +16,8 @@ export class RegisterComponent {
   formError: String ="";
 
   //Inject class Router dan service authentication
+  router: Router = inject(Router);
+  authService: AuthenticationService = inject (AuthenticationService);
 
   constructor(private fb: FormBuilder) {
     this.registerForm = this.fb.group({
@@ -41,6 +45,16 @@ export class RegisterComponent {
 
       console.log('Form submitted', formData);
       //Panggil method submitRegister()
+      this.authService.submitRegister(this.registerForm).then((res)=>{
+        if(res.message != null){
+          this.formError = res.message;
+        }else if(res.token !=null){
+          //this.authService.saveToken(res.token);
+          this.router.navigateByUrl('/');
+        }else{
+          this.formError = 'Register failed please try again';
+        }
+      });
     } else {
       this.formError = 'All fields are required, please try again';
       //console.log('Form is not valid');
